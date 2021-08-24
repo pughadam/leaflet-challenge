37b3250c15c1eb780a5a1d630b27854a8d73b40c
Adam Pugh <pughadam@hotmail.com>
2021-08-24 16:53:12 -0700

    Added in some markers for the visual

--- static/js/logic.js
+++ static/js/logic.js
@@ -3,5 +3,55 @@
 // This gets inserted into the div with an id of 'map'
 var myMap = L.map("map", {
     center: [45.52, -122.67],
-    zoom: 13
-  });
+    zoom: 5
+});
+
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+
+// Use this link to get the geojson data.
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Grabbing our GeoJSON data..
+d3.json(link).then(function (data) {
+    // Creating a GeoJSON layer with the retrieved data
+    
+
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(myMap);
+
+    function getColor(depth) {
+        if depth <= 10;
+            return 
+        if depth 
+    }
+});
+
+
+// function earthquakeLayer (feature, layer) {
+//     layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" new Date(feature.properties.time) + "</p>";
+
+// var earthquake = L.geoJson(earthquakeData, {
+//     OnEachFeature : earthquakeLayer});
