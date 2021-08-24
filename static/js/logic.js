4726953c9f5663a5c09d1ecbf5a103fccdc2ec76
Adam Pugh <pughadam@hotmail.com>
2021-08-24 10:52:38 -0700

    Added in var myMap and starting to build layers

---
+++ static/js/logic.js
@@ -0,0 +1,7 @@
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 13
+  });
