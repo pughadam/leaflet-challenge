c31abb8e8bd8002df1a51e59fea31f4ad281d754
Adam Pugh <pughadam@hotmail.com>
2021-08-25 21:18:21 -0700

    Got colors and size on the bubbles

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,14 +1,9 @@
-//Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
+
 var myMap = L.map("map", {
     center: [45.52, -122.67],
     zoom: 5
 });
 
-
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -18,48 +13,50 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: API_KEY
 }).addTo(myMap);
 
-
 // Use this link to get the geojson data.
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Grabbing our GeoJSON data..
-d3.json(link).then(function (data) {
 
-    function getColor(magnitude) {
-        switch (true) {
-            case mag > 5:
-                return "#dcf400";
-            case mag > 4:
-                return "#f7db11";
-            case mag > 3:
-                return "#fdb72a";
-            case mag > 2:
-                return "#fca35d";
-            case mag > 1:
-                return "#ff5f65";
-            default:
-                return "#98ee00"
+d3.json(link).then(function (data) {
+    function getColor(depth) {
+        if (depth < 1) {
+            return "#A3F600"
+        }
+        else if (depth < 5) {
+            return "#DCF400"
+        }
+        else if (depth < 10) {
+            return "#F7DB11"
+        }
+        else if (depth < 25) {
+            return "#FDB72A"
+        }
+        else if (depth < 50) {
+            return "#FCA35D"
+        }
+        else {
+            return "#FF5F65"
         }
     }
 
     function getRadius(magnitude) {
-        if (magnitude === 0) {
-            return 1;
-        }
-        return magnitude * 4;
+        return (magnitude) * 5;
     }
 
     L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
+        pointToLayer: 
+        function (feature, latlng) {
             var geojsonMarkerOptions = {
                 radius: 8,
-                fillColor: "#00000", //getColor(feature.properties.mag), 
+                fillColor: getColor(feature.properties.mag), //"#00000",
                 color: "#000",
-                // radius: getRadius(feature.properties.mag),
+                radius: getRadius(feature.properties.mag),
                 weight: 1,
                 opacity: 1,
-                fillOpacity: 0.8
-            };
+                fillOpacity: 1,
+            }
             return L.circleMarker(latlng, geojsonMarkerOptions);
-        }.addTo(myMap),
+        }
+    }).addTo(myMap);
+
 });
---
+++ static/js/logic_2.js
@@ -0,0 +1,91 @@
+//Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+    center: [45.52, -122.67],
+    zoom: 5
+});
+
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+
+// Use this link to get the geojson data.
+var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// function markerSize(magnitude) {
+//     return (magnitude) * 5;
+// }
+
+function getColor(depth) {
+    if (depth < 1) {
+        return "#A3F600"
+    }
+    else if (depth < 10) {
+        return "#DCF400"
+    }
+    else if (depth < 25) {
+        return "#F7DB11"
+    }
+    else if (depth < 50) {
+        return "#FDB72A"
+    }
+    else if (depth < 100) {
+        return "#FCA35D"
+    }
+    else {
+        return "#FF5F65"
+    }
+}
+
+// Grabbing our GeoJSON data..
+d3.json(link).then(function (data) {
+
+    // function getColor(magnitude) {
+    //     switch (true) {
+    //         case mag > 5:
+    //             return "#dcf400";
+    //         case mag > 4:
+    //             return "#f7db11";
+    //         case mag > 3:
+    //             return "#fdb72a";
+    //         case mag > 2:
+    //             return "#fca35d";
+    //         case mag > 1:
+    //             return "#ff5f65";
+    //         default:
+    //             return "#98ee00"
+    //     }
+    // }
+
+    // function getRadius(magnitude) {
+    //     if (magnitude === 0) {
+    //         return 1;
+    //     }
+    //     return magnitude * 4;
+    // }
+
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            // var geojsonMarkerOptions = {
+            //     radius: 8,
+            //     fillColor: "#00000", //getColor(feature.properties.mag), 
+            //     color: "#000",
+            //     // radius: getRadius(feature.properties.mag),
+            //     weight: 1,
+            //     opacity: 1,
+            //     fillOpacity: 0.8
+            // };
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }.addTo(myMap),
+    }
+});
